1476ad379fc618114af9cdb43494c1a3d8919db0
jasmeen95 <118770885+jasmeen95@users.noreply.github.com>
2022-11-21 14:25:40 -0800

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -14,6 +14,8 @@ my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/
 
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
-streamlit.multiselect("Pick some fruits", list(my_fruit_list.index),['Avocado','Strawberries'])
-streamlit.dataframe(my_fruit_list)
+fruit_selected=streamlit.multiselect("Pick some fruits", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruit_to_show=my_fruit_list.loc[fruits_selected]
+
+streamlit.dataframe(fruit_to_show)
 
