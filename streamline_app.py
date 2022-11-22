212f84bc0518624658de9600c0ce15054aef5753
jasmeen95 <118770885+jasmeen95@users.noreply.github.com>
2022-11-21 23:42:41 -0800

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -19,11 +19,11 @@ fruit_to_show=my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruit_to_show)
 
-streamlit.header('Fruitvice Fruit Advice!')
+streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
